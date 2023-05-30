03153c1d606d1943d2e316b18b214eaeba638371
pedrofranq96 <pedrofranq96@gmail.com>
2023-05-30 20:32:59 -0300

    Etapa 7: Implementando rotas

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,16 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './Routes'
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
+    <BrowserRouter>
+        <div className="app">
         <Logo/>
         <Nav />
-        <Main icon="home" title="início"
-            subtitle="Segundo Projeto do capítulo React - Web Moderno." />
+        <Routes/>
         <Footer/>
     </div>
+    </BrowserRouter>
+    
